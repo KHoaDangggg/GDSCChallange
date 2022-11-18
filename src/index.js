105a36d5ac8cfe190a7c56291197845393a2e496
KhoaDang <dangphuckhoa2003@gmail.com>
2022-11-18 22:26:23 +0700

    Change src folder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const $ = document.querySelector.bind(document);
 const menu = $('nav div.navOptions');
 const menuBar = $('#mobileMenu');
-
+const popup = document.getElementsByClassName('dropdown');
+const nav = $('nav');
+const navSvg = nav.getElementsByTagName('svg');
+const layer = $('.layer');
 // Mobile Navigation
 menuBar.onchange = function () {
-    if (menuBar.checked) {
-        menu.classList.display = 'block';
-        console.log(1);
-    } else {
-        menu.classList.display = 'none';
-        console.log(0);
-    }
+    menu.classList.toggle('fadeUp');
+    layer.classList.toggle('hidden');
 };
+
+// Nav
+window.addEventListener('scroll', () => {
+    let windowPosition = window.scrollY > 0;
+    nav.classList.toggle('scrolling-active', windowPosition);
+    Array.from(navSvg).forEach((element) => {
+        element.classList.toggle('filter', windowPosition);
+    });
+});
