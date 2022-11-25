bcfb1e6ae86df4cc6ba964bf425c6627419b77dc
KhoaDang <dangphuckhoa2003@gmail.com>
2022-11-26 02:18:50 +0700

    Xong 80%

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const popup = document.getElementsByClassName('dropdown');
 const nav = $('nav');
 const navSvg = nav.getElementsByTagName('svg');
 const layer = $('.layer');
+const ShortenBtn = $('.linkbox button');
+const ShortenText = $('.linkbox .input');
+const errorMessage = $('.linkbox i');
 // Mobile Navigation
 menuBar.onchange = function () {
     menu.classList.toggle('hidden', false);
@@ -20,3 +23,51 @@ window.addEventListener('scroll', () => {
         element.classList.toggle('filter', windowPosition);
     });
 });
+
+// Shorten btn
+ShortenBtn.onclick = function () {
+    if (ShortenText.querySelector('input').value === '') {
+        ShortenText.querySelector('i').style.display = 'block';
+        ShortenText.querySelector('input').style.border =
+            '2px solid var(--Red)';
+    } else {
+        ShortenText.querySelector('i').style.display = 'none';
+        ShortenText.querySelector('input').style.borderColor = '';
+    }
+};
+
+ShortenText.querySelector('input').onfocus = function () {
+    ShortenText.querySelector('i').style.display = 'none';
+    ShortenText.querySelector('input').style.borderColor = '';
+};
+
+let requestData = () => {};
+
+// Detect element is scrolled into view
+(function () {
+    var elements;
+    var windowHeight;
+
+    function init() {
+        elements = document.querySelectorAll('.notInView');
+        windowHeight = window.innerHeight;
+    }
+
+    function checkPosition() {
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            var positionFromTop = elements[i].getBoundingClientRect().top;
+
+            if (positionFromTop - windowHeight <= 0) {
+                element.classList.add('fade-in-element');
+                element.classList.remove('notInView');
+            }
+        }
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+
+    init();
+    checkPosition();
+})();
