2606e8fd1b76560b88e24a81560da830a82cd922
KhoaDang <dangphuckhoa2003@gmail.com>
2022-11-27 02:43:38 +0700

    UI Done

--- src/index.js
+++ src/index.js
@@ -6,68 +6,108 @@ const nav = $('nav');
 const navSvg = nav.getElementsByTagName('svg');
 const layer = $('.layer');
 const ShortenBtn = $('.linkbox button');
-const ShortenText = $('.linkbox .input');
+const ShortenText = $('.linkbox input');
 const errorMessage = $('.linkbox i');
-// Mobile Navigation
-menuBar.onchange = function () {
-    menu.classList.toggle('hidden', false);
-    menu.classList.toggle('fadeUp', !menuBar.checked);
-    layer.classList.toggle('hidden');
-};
+const links = document.querySelector('.links');
 
-// Nav
-window.addEventListener('scroll', () => {
-    let windowPosition = window.scrollY > 0;
-    nav.classList.toggle('scrolling-active', windowPosition);
-    Array.from(navSvg).forEach((element) => {
-        element.classList.toggle('filter', windowPosition);
-    });
-});
+const web = {
+    eventHandler() {
+        // Nav Scroll
+        window.addEventListener('scroll', () => {
+            let windowPosition = window.scrollY > 0;
+            nav.classList.toggle('scrolling-active', windowPosition);
+            Array.from(navSvg).forEach((element) => {
+                element.classList.toggle('filter', windowPosition);
+            });
+        });
 
-// Shorten btn
-ShortenBtn.onclick = function () {
-    if (ShortenText.querySelector('input').value === '') {
-        ShortenText.querySelector('i').style.display = 'block';
-        ShortenText.querySelector('input').style.border =
-            '2px solid var(--Red)';
-    } else {
-        ShortenText.querySelector('i').style.display = 'none';
-        ShortenText.querySelector('input').style.borderColor = '';
-    }
-};
-
-ShortenText.querySelector('input').onfocus = function () {
-    ShortenText.querySelector('i').style.display = 'none';
-    ShortenText.querySelector('input').style.borderColor = '';
-};
-
-let requestData = () => {};
-
-// Detect element is scrolled into view
-(function () {
-    var elements;
-    var windowHeight;
-
-    function init() {
-        elements = document.querySelectorAll('.notInView');
-        windowHeight = window.innerHeight;
-    }
-
-    function checkPosition() {
-        for (var i = 0; i < elements.length; i++) {
-            var element = elements[i];
-            var positionFromTop = elements[i].getBoundingClientRect().top;
-
-            if (positionFromTop - windowHeight <= 0) {
-                element.classList.add('fade-in-element');
-                element.classList.remove('notInView');
+        // Shorten btn
+        ShortenBtn.onclick = function () {
+            if (ShortenText.value === '') {
+                errorMessage.innerText = 'Please add a link';
+                ShortenText.style.border = '2px solid var(--Red)';
+            } else {
+                errorMessage.innerText = '';
+                ShortenText.style.borderColor = '';
+                web.getData(ShortenText.value);
+                ShortenText.value = '';
             }
+        };
+
+        ShortenText.onfocus = function () {
+            errorMessage.innerText = '';
+            ShortenText.style.borderColor = '';
+        };
+        // Mobile menu
+        menuBar.onchange = function () {
+            menu.classList.toggle('hidden', false);
+            menu.classList.toggle('fadeUp', !menuBar.checked);
+            layer.classList.toggle('hidden');
+        };
+    },
+    scrollIntoView() {
+        let elements;
+        let windowHeight;
+        function init() {
+            elements = document.querySelectorAll('.notInView');
+            windowHeight = window.innerHeight;
         }
+        function checkPosition() {
+            Array.from(elements).forEach((element) => {
+                let positionFromTop = element.getBoundingClientRect().top;
+
+                if (positionFromTop - windowHeight <= 0) {
+                    element.classList.add('fade-in-element');
+                    element.classList.remove('notInView');
+                }
+            });
+        }
+        window.onscroll = checkPosition;
+        window.onresize = init;
+        init();
+        checkPosition();
+    },
+    createHtmlElement(original, short) {
+        const div1 = document.createElement('div');
+        const p1 = document.createElement('p');
+        const div2 = document.createElement('div');
+        const p2 = document.createElement('p');
+        const copyBtn = document.createElement('button');
+        links.appendChild(div1);
+        p1.innerHTML = original;
+        div1.appendChild(p1);
+        div2.classList.add('newLink');
+        div1.appendChild(div2);
+        p2.innerHTML = short;
+        div2.appendChild(p2);
+        copyBtn.innerHTML = 'Copy';
+        copyBtn.onclick = function () {
+            navigator.clipboard.writeText(p2.innerHTML);
+            copyBtn.innerHTML = 'Copied!';
+            copyBtn.classList.add('copied');
+        };
+        div2.appendChild(copyBtn);
+    },
+    getData(data) {
+        fetch(`https://api.shrtco.de/v2/shorten?url=${data}`)
+            .then((res) => res.json())
+            .then((data) => web.shortLink(data));
+    },
+    shortLink(data) {
+        if (data.ok) {
+            const originalLink = data.result.original_link;
+            const shortLink = data.result.short_link;
+            web.createHtmlElement(originalLink, shortLink);
+        } else {
+            errorMessage.innerText = 'Invalid link';
+            ShortenText.style.border = '2px solid var(--Red)';
+        }
+    },
+    start() {
+        this.scrollIntoView();
+        this.eventHandler();
     }
+};
 
-    window.addEventListener('scroll', checkPosition);
-    window.addEventListener('resize', init);
-
-    init();
-    checkPosition();
-})();
+// Start
+web.start();
